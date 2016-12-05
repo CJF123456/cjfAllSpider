d6102a1748468b468342b5664735cfe33e84dfec
Administrator <1378022176@qq.com>
2016-12-05 11:05:33 +0800

    1.2

--- cjfAllSpiderTest/src/main/java/webmagic/test.java
+++ cjfAllSpiderTest/src/main/java/webmagic/test.java
@@ -8,6 +8,8 @@ public class test {
 	public static void main(String[] args) {
 		Spider.create(new MoocSpider())
 				.addUrl("http://www.imooc.com/user/newlogin/from_url/1005/")
+				
+				
 				.addPipeline(new ConsolePipeline()).thread(5).run();
 	}
 }
